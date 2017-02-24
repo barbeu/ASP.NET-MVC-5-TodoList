61d0922417730aefb28cbc168f161f62bd8b41b4
barbeu <yuriy.barbeu@gmail.com>
2017-02-24 20:10:56 +0300

    Create ASP.NET MVC project and connect to database.

---
+++ TodoList/Controllers/ListsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TodoList.lib.Entity;
+
+namespace TodoList.Controllers
+{
+    public class ListsController : Controller
+    {
+        private dbTodoListEntities dbContext;
+
+        public ListsController()
+        {
+            dbContext = new dbTodoListEntities();
+        }
+
+        //GET: Lists
+        public ActionResult Index()
+        {
+            return View(dbContext.tbLists.ToList());
+        }
+    }
+}
